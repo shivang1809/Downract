493d3929103a9b3653b10bb8cd501aa56cc0f554
shivang1809 <83906239+shivang1809@users.noreply.github.com>
2021-07-20 16:27:08 +0530

    Update loader.js

--- static/scripts/loader.js
+++ static/scripts/loader.js
@@ -1,6 +1,5 @@
 var preloader = document.getElementById("loader");
 
 function loadingFunction(){
-			loader.style.display = 'none';
-	                loader>div.style.display = 'none';
+			preloader.style.display = 'none';
 		};
