69fc726e09e02f47ed378c42ed2c38e4dbfcf702
shivang1809 <83906239+shivang1809@users.noreply.github.com>
2021-09-05 10:11:39 +0530

    Update index.js

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -14,6 +14,7 @@ function myFunction() {
     //document.getElementById("output").innerHTML = output;
     if (link_check === -1){
         alert("Invalid link");
+        x.style.display === "none"
         submitOK = "false"
         //return true ;
     }
